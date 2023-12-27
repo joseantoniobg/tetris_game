13d366631acc1de736accb1e470f092288fe5ee3
José Antônio <joseantonio1@hotmail.com.br>
2023-12-26 22:31:08 -0300

    feat: added read.md and Inverted L Piece

--- js/global-variables.js
+++ js/global-variables.js
@@ -45,6 +45,26 @@ const LPiece = [
    { x: 2, y: 0 },],
 ];
 
+const InvertedLPiece = [
+  [{ x: 1, y: 0 },
+   { x: 1, y: 1 },
+   { x: 1, y: 2 },
+   { x: 0, y: 2 },],
+  [{ x: 0, y: 0 },
+   { x: 0, y: 1 },
+   { x: 1, y: 1 },
+   { x: 2, y: 1 },],
+  [{ x: 0, y: 0 },
+   { x: 1, y: 0 },
+   { x: 0, y: 1 },
+   { x: 0, y: 2 },],
+  [{ x: 0, y: 0 },
+   { x: 1, y: 0 },
+   { x: 2, y: 0 },
+   { x: 2, y: 1 },],
+];
+
+
 const IPiece = [
   [{ x: 0, y: 0 },
    { x: 0, y: 1 },
@@ -104,7 +124,7 @@ const BlockPieceInverted = [
     { x: 1, y: 2 },],
 ];
 
-const tetrisPieces = [LPiece, IPiece, SquarePiece, BlockPiece, BlockPieceInverted, TPiece];
+const tetrisPieces = [LPiece, InvertedLPiece, IPiece, SquarePiece, BlockPiece, BlockPieceInverted, TPiece];
 
 const pieceYmovement = 0.25;
 
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Tetris Game
+## _A Tetris Game made entirely with JS_
+
+This project was coded between 12/23/2023 and 12/26/2023 by José Antônio Belquior Gomes.
+It was made from scratch. I was hyped by the movie, I confess.
+
+To experience, simply open the index.html in a modern browser.
+
+## Instructions
+
+- Space: starts game
+- left, right and down arrows: moves the pieces through the board
+- z: imediatlly sets the piece into it`s final destination (final straight position)
+
+## You will see in this project
+
+- JS Data Structures, such as arrays, two dimentional arrays
+- Classes to create the square (smallest part of the game) and pieces itself
+- Keyboard keydown handler
